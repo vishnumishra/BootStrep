cd5a7f9e45a751a2322c9ba61af930a41ee740f4
vishnumishra <vishnu143mishra@gmail.com>
2015-04-13 12:11:22 +0530

    bootstrep is almost completed

---
+++ Bootstrap Assignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bootstrap_Assignment.Startup))]
+namespace Bootstrap_Assignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
